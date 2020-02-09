1a77e33a62cd10299d084fde85b92af9ddeb1391
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-08 16:41:31 -0800

    refactored the header tests

--- src/component/header/header.test.js
+++ src/component/header/header.test.js
@@ -2,16 +2,28 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Header from './index'
 
+const setUp = (props={}) => {
+  const component = shallow(<Header {...props}/>);
+  return component;
+}
+
 describe('Header Component', () => {
 
-    it('It should render without errors', () => {
-      
-      const component = shallow(<Header/>) 
+    let component;
+    beforeEach(() => { // this runs before each test
+      component = setUp();
+    }) 
+
+    it('Should render without errors', () => {
       // this actually renders the component and debug basically 
       // Returns a string representation of the current shallow render tree for debugging purposes.
       // console.log(component.debug())
-     const wrapper = component.find('.headerComponent')
-     expect(wrapper.length).toBe(1)
+     const wrapper = component.find('.headerComponent'); // the .HeaderComponent is a class basically it finds the selector
+     expect(wrapper.length).toBe(1);
     })
 
+    it ("Should render a logo", () => {
+      const logo = component.find('.logoIMG');
+      expect(logo.length).toBe(1);
+    })
 })
