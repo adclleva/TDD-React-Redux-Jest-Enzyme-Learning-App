882ffe4a619e3fc5fe6a3f17201674cbf7b06e02
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-09 18:32:45 -0800

    Created the barebones code for the SharedButton component in order to go through the TDD approach for tests

---
+++ src/component/button/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import propTypes from 'prop-types'
+
+class SharedButton extends Component {
+  render() {
+
+    return (
+      <button>
+        
+      </button>
+    )
+  }
+}
+
+SharedButton.propTypes = {
+
+}
+
+export default SharedButton
