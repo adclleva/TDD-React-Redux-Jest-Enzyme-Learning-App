fb57a16b1a2868199601b40691f9ade8d1082347
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-10 14:07:58 -0800

    Created the ListItem component with its appropriate tests

--- src/component/listItem/index.js
+++ src/component/listItem/index.js
@@ -4,9 +4,20 @@ import PropTypes from 'prop-types'
 class ListItem extends Component {
   
   render() {
-    return (
-      <div>
+    const { title, description} = this.props
 
+    if (!title) { // if a title is not provided, it will return null(component) as a component
+      return null
+    }
+
+    return (
+      <div data-test-id="listItemComponent">
+        <h2 data-test-id="componentTitle">
+          {title}
+        </h2>
+        <p data-test-id="componentDescription">
+          {description}
+        </p>
       </div>
     )
   }
